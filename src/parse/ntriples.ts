edf4b1d2f4f719d66691c18a0a29031fb9a0b8ca
Xubai Wang <self@xubai.wang>
2022-08-28 10:47:14 +0800

    refactor: use createLanguage style

--- src/parse/ntriples.ts
+++ src/parse/ntriples.ts
@@ -1,5 +1,6 @@
 import {
   charWhere,
+  createLanguage,
   either,
   hex,
   many,
@@ -14,171 +15,53 @@ import {
   str,
   surrounded,
 } from "https://deno.land/x/combine@v0.0.9/mod.ts";
-import { BlankNode, DataFactory, Literal, NamedNode, Quad } from "../model.ts";
+import {
+  BlankNode,
+  DataFactory,
+  Literal,
+  NamedNode,
+  Quad,
+  QuadObject,
+  QuadPredicate,
+  QuadSubject,
+} from "../model.ts";
 
-const f = new DataFactory();
+/** The data factory */
+const F = new DataFactory();
 
-const uChar: Parser<string> = either(
-  mapJoin(
-    seq(
-      str(`\\u`),
-      mapJoin(repeat(4, hex())),
-    ),
-  ),
-  mapJoin(
-    seq(
-      str(`\\U`),
-      mapJoin(repeat(8, hex())),
-    ),
-  ),
-);
+/* The NTriples language type definition */
+type NTriplesLanguage = {
+  ntriplesDoc: Parser<Quad[]>;
+  triple: Parser<Quad>;
+  subject: Parser<QuadSubject>;
+  predicate: Parser<QuadPredicate>;
+  object: Parser<QuadObject>;
+  literal: Parser<Literal>;
+  LANGTAG: Parser<string>;
+  EOL: Parser<string>;
+  IRIREF: Parser<NamedNode>;
+  STRING_LITERAL_QUOTE: Parser<string>;
+  BLANK_NODE_LABEL: Parser<BlankNode>;
+  UCHAR: Parser<string>;
+  ECHAR: Parser<string>;
+  PN_CHARS_BASE: Parser<string>;
+  PN_CHARS_U: Parser<string>;
+  PN_CHARS: Parser<string>;
+  HEX: Parser<string>;
+};
 
-const eChar: Parser<string> = mapJoin(
+/** The NTriples language parsers */
+const ntripleLanguage = createLanguage<NTriplesLanguage>({
+  ntriplesDoc: S => map(
   seq(
-    str(`\\`),
-    regex(/[tbnrf"'\\]/, `expect [tbnrf"'\\]`),
-  ),
-);
-
-const iriRef: Parser<NamedNode> = map(
-  surrounded(
-    str(`<`),
-    mapJoin(
-      many(
-        either(
-          regex(/[^\u0000-\u0020<>"{}|^`\\]/, 'expect [^#x00-#x20<>"{}|^`\\]'),
-          uChar,
-        ),
-      ),
-    ),
-    str(`>`),
-  ),
-  (iri) => f.namedNode(iri),
-);
-
-const langTag: Parser<string> = map(
-  seq(
-    str(`@`),
-    regex(/[a-zA-Z]+(-[a-zA-Z0-9]+)*/, "expect [a-zA-Z]+ ('-' [a-zA-Z0-9]+)*"),
-  ),
-  ([, x]) => x,
-);
-
-const eol: Parser<string> = regex(/[\u000D\u000A]+/, `expect eol`);
-
-const stringLiteralQuote: Parser<string> = surrounded(
-  str(`"`),
-  mapJoin(
-    many(
-      oneOf(
-        regex(/[^\u0022\u005C\u000A\u000D]/, "expect [^#x22#x5c#xa#xd]"),
-        eChar,
-        uChar,
-      ),
-    ),
-  ),
-  str(`"`),
-);
-
-const pnCharsBase: Parser<string> = charWhere(
-  (c) =>
-    65 <= c && c <= 90 ||
-    97 <= c && c <= 122 ||
-    0xC0 <= c && c <= 0xD6 ||
-    0xD8 <= c && c <= 0xF6 ||
-    0xF8 <= c && c <= 0x02FF ||
-    0x370 <= c && c <= 0x37D ||
-    0x37F <= c && c <= 0x1FFF ||
-    0x200C <= c && c <= 0x200D ||
-    0x2070 <= c && c <= 0x218F ||
-    0x2C00 <= c && c <= 0x2FEF ||
-    0x3001 <= c && c <= 0xD7FF ||
-    0xF900 <= c && c <= 0xFDCF ||
-    0xFDF0 <= c && c <= 0xFFFD ||
-    0x10000 <= c && c <= 0xEFFFF,
-);
-
-const pnCharsU: Parser<string> = oneOf(
-  pnCharsBase,
-  str(`_`),
-  str(`:`),
-);
-
-const pnChars: Parser<string> = oneOf(
-  pnCharsU,
-  str(`-`),
-  regex(/[0-9]/, `expect [0-9]`),
-  str(`\u00B7`),
-  regex(/[\u0300-\u036F]/, `expect [#x0300-#x036F]`),
-  regex(/[\u203F-\u2040]/, `expect [#x203F-#x2040]`),
-);
-
-const blankNodeLabel: Parser<BlankNode> = map(
-  seq(
-    str(`_:`),
-    either(pnCharsU, regex(/[0-9]/, `expect [0-9]`)),
-    optional(seq(
-      mapJoin(many(either(pnChars, str(`.`)))),
-      pnChars,
-    )),
-  ),
-  ([, a, b]) => f.blankNode(a + b ?? ""),
-);
-
-const literal: Parser<Literal> = map(
-  seq(
-    stringLiteralQuote,
-    optional(
-      either(
-        map(seq(str(`^^`), iriRef), ([, iri]) => iri),
-        langTag,
-      ),
-    ),
-  ),
-  ([value, opt]) => {
-    return f.literal(value, opt ?? undefined);
-  },
-);
-
-const subject: Parser<NamedNode | BlankNode> = either(
-  iriRef,
-  blankNodeLabel,
-);
-
-const predicate: Parser<NamedNode> = iriRef;
-
-const object: Parser<NamedNode | Literal | BlankNode> = either(
-  iriRef,
-  either(
-    blankNodeLabel,
-    literal,
-  ),
-);
-
-const triple: Parser<Quad> = map(
-  seq(
-    many(str(" ")),
-    subject,
-    many(str(" ")),
-    predicate,
-    many(str(" ")),
-    object,
-    many(str(" ")),
-    str(`.`),
-  ),
-  ([, s, , p, , o]) => f.quad(s, p, o, f.defaultGraph()),
-);
-
-const ntriplesDoc: Parser<Quad[]> = map(
-  seq(
-    optional(triple),
+    optional(S.triple),
     many(
       seq(
-        eol,
-        triple,
+        S.EOL,
+        S.triple,
       ),
     ),
-    optional(eol),
+    optional(S.EOL),
   ),
   ([a, b]) => {
     const result = [];
@@ -190,6 +73,158 @@ const ntriplesDoc: Parser<Quad[]> = map(
     }
     return result;
   },
-);
+),
+  triple: S => map(
+  seq(
+    many(str(" ")),
+    S.subject,
+    many(str(" ")),
+    S.predicate,
+    many(str(" ")),
+    S.object,
+    many(str(" ")),
+    str(`.`),
+  ),
+  ([, s, , p, , o]) => F.quad(s, p, o, F.defaultGraph()),
+),
+  subject: S => either(
+  S.IRIREF,
+  S.BLANK_NODE_LABEL,
+),
+  predicate: S => S.IRIREF,
+  object: S => either(
+  S.IRIREF,
+  either(
+    S.BLANK_NODE_LABEL,
+    S.literal,
+  ),
+),
+  literal: S => map(
+  seq(
+    S.STRING_LITERAL_QUOTE,
+    optional(
+      either(
+        map(seq(str(`^^`), S.IRIREF), ([, iri]) => iri),
+        S.LANGTAG,
+      ),
+    ),
+  ),
+  ([value, opt]) => {
+    return F.literal(value, opt ?? undefined);
+  },
+),
+  LANGTAG: () =>
+    map(
+      seq(
+        str(`@`),
+        regex(
+          /[a-zA-Z]+(-[a-zA-Z0-9]+)*/,
+          "expect [a-zA-Z]+ ('-' [a-zA-Z0-9]+)*",
+        ),
+      ),
+      ([, x]) => x,
+    ),
+  EOL: () => regex(/[\u000D\u000A]+/, `expect eol`),
+  IRIREF: (S) =>
+    map(
+      surrounded(
+        str(`<`),
+        mapJoin(
+          many(
+            either(
+              regex(
+                /[^\u0000-\u0020<>"{}|^`\\]/,
+                'expect [^#x00-#x20<>"{}|^`\\]',
+              ),
+              S.UCHAR,
+            ),
+          ),
+        ),
+        str(`>`),
+      ),
+      (iri) => F.namedNode(iri),
+    ),
+  STRING_LITERAL_QUOTE: S =>
+    surrounded(
+      str(`"`),
+      mapJoin(
+        many(
+          oneOf(
+            regex(/[^\u0022\u005C\u000A\u000D]/, "expect [^#x22#x5c#xa#xd]"),
+            S.ECHAR,
+            S.UCHAR,
+          ),
+        ),
+      ),
+      str(`"`),
+    ),
+  BLANK_NODE_LABEL: S => map(
+  seq(
+    str(`_:`),
+    either(S.PN_CHARS_U, regex(/[0-9]/, `expect [0-9]`)),
+    optional(seq(
+      mapJoin(many(either(S.PN_CHARS, str(`.`)))),
+      S.PN_CHARS,
+    )),
+  ),
+  ([, a, b]) => F.blankNode(a + b ?? ""),
+),
+  UCHAR: () =>
+    either(
+      mapJoin(
+        seq(
+          str(`\\u`),
+          mapJoin(repeat(4, hex())),
+        ),
+      ),
+      mapJoin(
+        seq(
+          str(`\\U`),
+          mapJoin(repeat(8, hex())),
+        ),
+      ),
+    ),
+  ECHAR: () =>
+    mapJoin(
+      seq(
+        str(`\\`),
+        regex(/[tbnrf"'\\]/, `expect [tbnrf"'\\]`),
+      ),
+    ),
+  PN_CHARS_BASE: () =>
+    charWhere(
+      (c) =>
+        65 <= c && c <= 90 ||
+        97 <= c && c <= 122 ||
+        0xC0 <= c && c <= 0xD6 ||
+        0xD8 <= c && c <= 0xF6 ||
+        0xF8 <= c && c <= 0x02FF ||
+        0x370 <= c && c <= 0x37D ||
+        0x37F <= c && c <= 0x1FFF ||
+        0x200C <= c && c <= 0x200D ||
+        0x2070 <= c && c <= 0x218F ||
+        0x2C00 <= c && c <= 0x2FEF ||
+        0x3001 <= c && c <= 0xD7FF ||
+        0xF900 <= c && c <= 0xFDCF ||
+        0xFDF0 <= c && c <= 0xFFFD ||
+        0x10000 <= c && c <= 0xEFFFF,
+    ),
+  PN_CHARS_U: (S) =>
+    oneOf(
+      S.PN_CHARS_BASE,
+      str(`_`),
+      str(`:`),
+    ),
+  PN_CHARS: (S) =>
+    oneOf(
+      S.PN_CHARS_U,
+      str(`-`),
+      regex(/[0-9]/, `expect [0-9]`),
+      str(`\u00B7`),
+      regex(/[\u0300-\u036F]/, `expect [#x0300-#x036F]`),
+      regex(/[\u203F-\u2040]/, `expect [#x203F-#x2040]`),
+    ),
+  HEX: () => hex(),
+});
 
-export default ntriplesDoc;
+export default ntripleLanguage.ntriplesDoc;
