92fb5cb98c814338ccca7e4f1a72f53ceff7986c
Xubai Wang <self@xubai.wang>
2023-03-28 13:39:43 +0800

    refactor: rename vocab to vocabs

--- vocab/mod.ts
+++
@@ -1,5 +0,0 @@
-export * as rdf from "./rdf.ts";
-export * as rdfs from "./rdfs.ts";
-export * as xdf from "./xsd.ts";
-export * as owl from "./owl.ts";
-export * as dc from "./dc.ts";
---
+++ vocabs.ts
@@ -0,0 +1,5 @@
+export { default as rdf } from "./vocabs/rdf.ts";
+export { default as rdfs } from "./vocabs/rdfs.ts";
+export { default as xsd } from "./vocabs/xsd.ts";
+export { default as owl } from "./vocabs/owl.ts";
+export { default as dc } from "./vocabs/dc.ts";
