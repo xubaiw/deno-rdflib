a751ead0974e1bb8cd5b2a4e689a970ee8c2a111
Xubai Wang <18016038327@189.cn>
2023-07-18 17:51:41 +0800

    Fix relative import of model in vocab

--- vocabs/utils.ts
+++ vocabs/utils.ts
@@ -1,4 +1,4 @@
-import { NamedNode } from "https://denopkg.com/xubaiw/deno-rdflib@main/mod.ts";
+import { NamedNode, namedNode } from "../model.ts";
 import { zipObj } from "npm:rambda";
 
 export function vocab<const T extends string>(
@@ -7,13 +7,6 @@ export function vocab<const T extends string>(
 ): { [key in T]: NamedNode } {
   return zipObj(
     names,
-    names.map((n) => namedNode(prefix, n)),
+    names.map((n) => namedNode(prefix + n)),
   );
-}
-
-function namedNode(prefix: string, name: string): NamedNode {
-  return {
-    tag: "named",
-    iri: prefix + name,
-  };
 }
